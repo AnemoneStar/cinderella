ce1262d1b1a1c02afbfb30d3f725e798bd0bebbb
petitsurume <git@surume.tk>
2017-10-15 00:37:03 +0900

    load dotenv

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
 import twitter
 import requests
+import dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+dotenv.load_dotenv(dotenv_path)
+
 required_envs = [
     "TWITTER_CONSUMER_KEY",
     "TWITTER_CONSUMER_SECRET",
